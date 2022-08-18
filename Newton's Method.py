bafd08b2bfe09c4d92f1c401e7376d7259ccd3e5
Adam Ghatta <67446944+RT5x@users.noreply.github.com>
2022-08-18 16:38:33 -0700

    Update Newton's Method.py

--- Newton's Method.py
+++ Newton's Method.py
@@ -4,9 +4,9 @@ print("Newton's Method\n")
 
 def f(x):
     return x**2 + x
-  #Any polynomial function here
+  #Any polynomial function here to define f(x)
 
-def d_f(x):
+def d_f(x):         #Definine f'(x)
     h = 1e-5
     return (f(x+h)-f(x-h))/(2*h)
 
@@ -32,7 +32,7 @@ def N(x0, k, f):
   
     elif k > 1 and (k%2 == 0 or (k-1)%2 ==0):
       i = 0
-      while i < k:
+      while i < k:  #Newton's method
         alist[i+1] = alist[i] - ((f(alist[i]))/(d_f(alist[i])))
         alist.append(alist[i+1])
         print(alist[i+1])
