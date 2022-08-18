f3493cf2fd68a6e4346e44cf5713159443c1322e
Adam Ghatta <67446944+RT5x@users.noreply.github.com>
2022-08-18 16:41:27 -0700

    Update Newton's Method.py

--- Newton's Method.py
+++ Newton's Method.py
@@ -1,7 +1,5 @@
 import math
-
 print("Newton's Method\n")
-
 def f(x):
     return x**2 + x
   #Any polynomial function here to define f(x)
@@ -9,27 +7,21 @@ def f(x):
 def d_f(x):         #Definine f'(x)
     h = 1e-5
     return (f(x+h)-f(x-h))/(2*h)
-
 #Input initial guess
 x0 = float(input("Root Guess: "))
-
 #Iterations needed to make approximation
 k = int(input("Number of iterations (positive integer): "))
 alist = [x0]
 #Newton's method
 def N(x0, k, f):
-  
     if k == 1:
       guess = x0 - ((f(x0))/(d_f(x0)))
       return guess
       print(guess)
-
     elif k < 0:
       print("Number of iterations must be a positive integer.")
-
     elif k%2 != 0 or (k-1)%2 !=0:
       print("Number of iterations must be a positive integer")
-  
     elif k > 1 and (k%2 == 0 or (k-1)%2 ==0):
       i = 0
       while i < k:  #Newton's method
